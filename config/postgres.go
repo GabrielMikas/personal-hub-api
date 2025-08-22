4d8e1bcfbdfbb54fc71ced6ef94c6385a91138bd
GabrielMikas <112135725+GabrielMikas@users.noreply.github.com>
2025-08-22 00:54:46 -0300

    chore: wip

--- config/postgres.go
+++ config/postgres.go
@@ -11,18 +11,22 @@ import (
 )
 
 func initializePostgres() (*gorm.DB, error) {
+
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println("Error loading environment variables:", err)
 	}
+
 	dsn := os.Getenv("DB_DSN")
 	if dsn == "" {
 		fmt.Println("Error: DB_DSN not set in .env")
 	}
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println("Error opening database:", err)
 	}
+
 	err = db.AutoMigrate(&schemas.Investment{})
 	if err != nil {
 		fmt.Println("Error running migration:", err)
