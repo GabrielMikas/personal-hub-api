c7be7f9c2b1e569aa91b97795c41c1fed253dbdc
GabrielMikas <112135725+GabrielMikas@users.noreply.github.com>
2025-08-23 11:59:39 -0300

    feat(investments): implemented get request

--- handlers/investments/getHandler.go
+++ handlers/investments/getHandler.go
@@ -3,11 +3,18 @@ package investments_handler
 import (
 	"net/http"
 
+	response_handlers "github.com/GabrielMikas/personal-hub-api/handlers/responses"
+	"github.com/GabrielMikas/personal-hub-api/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func GetHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "WIP",
-	})
+	investments := []schemas.Investment{}
+	if err := db.Find(&investments).Error; err != nil {
+		response_handlers.FailMessage(c, http.StatusInternalServerError, err.Error())
+	}
+	msg := gin.H{
+		"data": investments,
+	}
+	response_handlers.SuccessMessage(c, http.StatusOK, msg)
 }
